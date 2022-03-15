e84a61b2c6e44534abf673e350d50b42ed61453d
2now <kj109888@gmail.com>
2022-03-16 01:30:57 +0900

    💄 Add Main/Banner

---
+++ front/src/container/Main/Banner.tsx
@@ -0,0 +1,64 @@
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+
+import data from '@/data/index';
+
+const imageCss = {
+  width: '25rem',
+  height: '30rem',
+  mt: '10rem',
+  ml: '14rem',
+};
+
+const buttonCss = {
+  width: '10rem',
+};
+
+function Banner() {
+  return (
+    <Box sx={{ display: 'flex', pb: '5rem' }}>
+      <Box sx={{ ml: '20rem' }}>
+        <Typography
+          sx={{ mt: '13rem' }}
+          variant="h2"
+          component="div"
+          gutterBottom
+        >
+          Donate for your artist.
+        </Typography>
+        <Typography
+          sx={{ mt: '3rem', ml: '3rem' }}
+          variant="h2"
+          component="div"
+          gutterBottom
+        >
+          Create your Artwork to NFT
+        </Typography>
+
+        <Box sx={{ mt: '11rem', ml: '3.5rem' }}>
+          <Button sx={buttonCss} variant="contained" size="large">
+            Explore
+          </Button>
+
+          <Button
+            sx={{ ml: '1.5rem', ...buttonCss }}
+            variant="outlined"
+            size="large"
+          >
+            Create
+          </Button>
+        </Box>
+      </Box>
+
+      <Card sx={imageCss}>
+        <CardMedia component="img" height="100%" image={data[0]} />
+      </Card>
+    </Box>
+  );
+}
+
+export default Banner;
---
+++ front/src/container/Main/index.tsx
@@ -0,0 +1,7 @@
+import Banner from './Banner';
+
+function index() {
+  return <Banner />;
+}
+
+export default index;
