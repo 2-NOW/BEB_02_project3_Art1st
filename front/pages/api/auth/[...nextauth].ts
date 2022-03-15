ed5dba390de727884a5a63beccfc84674b739fab
2now <kj109888@gmail.com>
2022-03-15 16:27:07 +0900

    🐛 Fix a bug
    임시 api와 prisma schema의 env 부분을 수정했습니다.

--- front/pages/api/auth/[...nextauth].ts
+++ front/pages/api/auth/[...nextauth].ts
@@ -30,24 +30,8 @@ export default NextAuth({
   },
   adapter: PrismaAdapter(prisma),
   secret: SECRET,
-  session: {
-    strategy: 'jwt',
-  },
   theme: {
     colorScheme: 'light',
   },
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
+  callbacks: {},
 });
--- front/pages/api/nfts.ts
+++ front/pages/api/nfts.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
+import { getToken } from 'next-auth/jwt';
 import prisma from '@/utils/prisma/index';
 
 interface Data {}
@@ -8,25 +9,24 @@ export default async function createNft(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // session 찾아서 user를 특정하는 api입니다.
+  // 임시로 expires로 session record를 찾았는데
+  // 참고할 다른 id값이 있거나 다른 방법이 있으면 수정하겠습니다.
   const session = await getSession({ req });
 
   if (!session) {
     return res.status(401).json({ unauthorized: true });
   }
 
-  console.log('session123', session.accessToken);
+  const sessionRecord = await prisma.session.findFirst({
+    where: { expires: session.expires },
+  });
+  console.log('session record: ', sessionRecord);
 
-  if (typeof session.accessToken === 'string') {
-    const accountRecord = await prisma.account.findMany({
-      where: { access_token: session.accessToken },
-    });
-    console.log(accountRecord, '12333');
-
-    const user = await prisma.user.findMany({
-      where: { id: accountRecord.userId },
-    });
-    console.log(user);
-  }
+  const user = await prisma.user.findFirst({
+    where: { id: sessionRecord?.userId },
+  });
+  console.log('user: ', user);
 
   res.status(200).send('end');
 }
