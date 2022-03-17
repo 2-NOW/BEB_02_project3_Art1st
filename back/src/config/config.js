9197799d411ca53a3fcf830a56835e2f1618e00e
jsg0629 <tnrms513@gmail.com>
2022-03-17 19:50:44 +0900

    :heavy_plus_sign: Add Login API and Create Mnemonic Wallet
    로그인 관련 API, 니모닉 지갑 생성 구현

--- back/src/config/config.js
+++ back/src/config/config.js
@@ -1,33 +1,67 @@
 import dotenv from 'dotenv';
 dotenv.config();
-const env = process.env;
+console.log(`환경변수 ${process.env.MYSQL_USER}`);
+
+// // MySQL & Sequelize configuration
+// const development = {
+//   username: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PW,
+//   database: process.env.MYSQL_NAME,
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+//   port: 3306, // mysql은 3306 port 사용
+//   timezone: "+09:00",
+//   //logging: true
+// };
+
+// const production = {
+//   username: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PW,
+//   database: 'Project5_test',
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+// };
+
+// const test = {
+//   username: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PW,
+//   database: "Project5_production",
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+// };
+
+// export default { development, production, test };
+
+
+
 
 // MySQL & Sequelize configuration
 const development = {
-  username: env.MYSQL_USER,
-  password: env.MYSQL_PW,
-  database: env.MYSQL_NAME,
-  host: env.MYSQL_HOST,
+  username: 'root',
+  password: '153945',
+  database: 'Project5',
+  host: "127.0.0.1",
   dialect: "mysql",
-  port: env.MYSQL_PORT,
+  port: 3306, // mysql은 3306 port 사용
   timezone: "+09:00",
   //logging: true
 };
 
 const production = {
-  username: env.MYSQL_USER,
-  password: env.MYSQL_PW,
-  database: env.MYSQL_NAME,
-  host: env.MYSQL_HOST,
+  username: 'root',
+  password: '153945',
+  database: 'Project5_test',
+  host: "127.0.0.1",
   dialect: "mysql",
 };
 
 const test = {
-  username: env.MYSQL_USER,
-  password: env.MYSQL_PW,
-  database: env.MYSQL_NAME,
-  host: env.MYSQL_HOST,
+  username: 'root',
+  password: '153945',
+  database: "Project5_production",
+  host: "127.0.0.1",
   dialect: "mysql",
 };
 
 export default { development, production, test };
+
--- back/src/loaders/express.js
+++ back/src/loaders/express.js
@@ -1,9 +1,26 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
-
+import express from 'express';
+import session from 'express-session';
 import router from '../api/index.js';
 
 export default async ({ app }) => {
+    app.use(
+        session({
+          secret: 'art1stProject5',  //암호화하는 데 쓰일 키
+          resave: false, // 세션을 언제나 저장할지 설정함 (?)
+          saveUninitialized: true, //세션이 저장되기 전 uninitialized 상태로 미리 만들어 저장
+          cookie: {
+            domain: `localhost`,  // 클라는 서버도메인이 설정과 같아야 쿠키전송가능
+            path: '/', // 해당 path를 만족하면 쿠키 보내줌
+            maxAge: 24 * 6 * 60 * 10000,
+            sameSite: 'none',
+            httpOnly: true, // 스크립트 접근 막기
+            secure: true, // http 통신 쿠키 허용
+          },
+        })
+      );
+    app.use(express.json({ strict : true }))
     app.use(bodyParser.urlencoded({extended: true})); // 중첩된 객체 파싱 가능
     app.use(bodyParser.json());
     app.use(cors({
