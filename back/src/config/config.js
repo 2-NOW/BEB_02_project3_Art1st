68c028145d69261788ab3a52172362995b8c9a7c
jsg0629 <tnrms513@gmail.com>
2022-03-17 20:47:56 +0900

    :bug: fix a bug env environment variable

--- back/src/config/config.js
+++ back/src/config/config.js
@@ -4,9 +4,9 @@ console.log(`환경변수 ${process.env.MYSQL_USER}`);
 
 // MySQL & Sequelize configuration
 const development = {
-  username: 'root',
-  password: '153945',
-  database: 'Project5',
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PW,
+  database: process.env.MYSQL_NAME,
   host: "127.0.0.1",
   dialect: "mysql",
   port: 3306, // mysql은 3306 port 사용
@@ -15,20 +15,53 @@ const development = {
 };
 
 const production = {
-  username: 'root',
-  password: '153945',
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PW,
   database: 'Project5_test',
   host: "127.0.0.1",
   dialect: "mysql",
 };
 
 const test = {
-  username: 'root',
-  password: '153945',
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PW,
   database: "Project5_production",
   host: "127.0.0.1",
   dialect: "mysql",
 };
 
 export default { development, production, test };
+
+
+
+
+// // MySQL & Sequelize configuration
+// const development = {
+//   username: 'root',
+//   password: '153945',
+//   database: 'Project5',
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+//   port: 3306, // mysql은 3306 port 사용
+//   timezone: "+09:00",
+//   //logging: true
+// };
+
+// const production = {
+//   username: 'root',
+//   password: '153945',
+//   database: 'Project5_test',
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+// };
+
+// const test = {
+//   username: 'root',
+//   password: '153945',
+//   database: "Project5_production",
+//   host: "127.0.0.1",
+//   dialect: "mysql",
+// };
+
+// export default { development, production, test };
 
