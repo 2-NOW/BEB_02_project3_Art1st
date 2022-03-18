950d8dad4bfe5384f9dc38e5432e263e0d3cf1b2
Seomingyun <aa123490@gmail.com>
2022-03-18 14:36:08 +0900

    :wrench: Modify configuration file and env.example file
    DB config 설정 파일들 원래대로 다시 수정

--- back/src/config/config.js
+++ back/src/config/config.js
@@ -1,67 +1,33 @@
 import dotenv from 'dotenv';
 dotenv.config();
-console.log(`환경변수 ${process.env.MYSQL_USER}`);
+const env = process.env;
 
 // MySQL & Sequelize configuration
 const development = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PW,
-  database: process.env.MYSQL_NAME,
-  host: "127.0.0.1",
+  username: env.MYSQL_USER,
+  password: env.MYSQL_PW,
+  database: env.MYSQL_NAME,
+  host: env.MYSQL_HOST,
   dialect: "mysql",
-  port: 3306, // mysql은 3306 port 사용
-  timezone: "+09:00",
+  port: env.MYSQL_PORT,
+  //timezone: "+09:00",
   //logging: true
 };
 
 const production = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PW,
-  database: 'Project5_test',
-  host: "127.0.0.1",
+  username: env.MYSQL_USER,
+  password: env.MYSQL_PW,
+  database: env.MYSQL_NAME,
+  host: env.MYSQL_HOST,
   dialect: "mysql",
 };
 
 const test = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PW,
-  database: "Project5_production",
-  host: "127.0.0.1",
+  username: env.MYSQL_USER,
+  password: env.MYSQL_PW,
+  database: env.MYSQL_NAME,
+  host: env.MYSQL_HOST,
   dialect: "mysql",
 };
 
 export default { development, production, test };
-
-
-
-
-// // MySQL & Sequelize configuration
-// const development = {
-//   username: 'root',
-//   password: '153945',
-//   database: 'Project5',
-//   host: "127.0.0.1",
-//   dialect: "mysql",
-//   port: 3306, // mysql은 3306 port 사용
-//   timezone: "+09:00",
-//   //logging: true
-// };
-
-// const production = {
-//   username: 'root',
-//   password: '153945',
-//   database: 'Project5_test',
-//   host: "127.0.0.1",
-//   dialect: "mysql",
-// };
-
-// const test = {
-//   username: 'root',
-//   password: '153945',
-//   database: "Project5_production",
-//   host: "127.0.0.1",
-//   dialect: "mysql",
-// };
-
-// export default { development, production, test };
-
