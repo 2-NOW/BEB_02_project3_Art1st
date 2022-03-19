0ca54a469a15fae374b183e379a195d7aefe7115
2now <kj109888@gmail.com>
2022-03-19 18:26:10 +0900

    💄 Add userdetail/profile

--- front/src/container/UserDetail/Profile/index.tsx
+++ front/src/container/UserDetail/Profile/index.tsx
@@ -1,5 +1,56 @@
+import Image from 'next/image';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import EmailIcon from '@mui/icons-material/Email';
+
+import data from '@/data/index';
+
 function index() {
-  return <div>Enter</div>;
+  const testData = data[41];
+
+  const profileImageCss = {
+    position: 'relative',
+    width: '100%',
+    height: '15rem',
+  };
+
+  return (
+    <>
+      <Box sx={profileImageCss}>
+        <Image src={testData} quality={100} layout="fill" objectFit="cover" />
+      </Box>
+      <Typography sx={{ ml: '0.5rem', mt: '2rem' }} variant="h5">
+        Artist
+      </Typography>
+
+      <Typography sx={{ mt: '1rem' }} variant="subtitle1">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit natus
+        nisi quos, dolorum corrupti voluptate! Hic dicta quo nihil eligendi?
+        Iure deleniti modi facere ipsum ullam nesciunt quis soluta distinctio.
+      </Typography>
+
+      <Box sx={{ mt: '2rem' }}>
+        <IconButton>
+          <InstagramIcon />
+        </IconButton>
+        <IconButton>
+          <TwitterIcon />
+        </IconButton>
+        <IconButton>
+          <EmailIcon />
+        </IconButton>
+      </Box>
+
+      <Button sx={{ mt: '2rem', width: '100%' }} variant="contained">
+        Donate
+      </Button>
+    </>
+  );
 }
 
 export default index;
--- front/src/container/UserDetail/index.tsx
+++ front/src/container/UserDetail/index.tsx
@@ -8,10 +8,19 @@ function index() {
   return (
     <>
       <Grid container>
-        <Grid item xl={4}>
-          <Profile />
+        <Grid item xs={12} xl={4}>
+          <Box
+            sx={{
+              width: '15rem',
+              position: 'sticky',
+              top: '10rem',
+              m: '0 auto 0 14vw',
+            }}
+          >
+            <Profile />
+          </Box>
         </Grid>
-        <Grid item xl={8}>
+        <Grid item xs={12} xl={8}>
           <Box sx={{ m: '6rem 16vw 0 auto' }}>
             <Content />
           </Box>
