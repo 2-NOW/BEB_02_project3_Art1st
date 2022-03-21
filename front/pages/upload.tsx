3afbb1796806cb8aeeb1654224df3a7d08055be1
sungyeon1 <sungyeon0320@gmail.com>
2022-03-21 09:57:22 +0900

    :lipstick: Add tag

--- front/pages/upload.tsx
+++ front/pages/upload.tsx
@@ -1,5 +1,11 @@
+import Navbar from "@/components/Layout/Navbar/index";
 import Upload from "@/container/Upload/index";
 
 export default function Create() {
-  return <Upload />;
+  return (
+    <>
+      <Navbar />
+      <Upload />
+    </>
+  );
 }
--- front/src/container/Upload/fragment/Uploadbtn.tsx
+++ front/src/container/Upload/fragment/Uploadbtn.tsx
@@ -1,12 +1,12 @@
 import { Typography, Button } from "@mui/material";
 
-const SignInCss = {
+const UploadCss = {
   display: "inline-block",
-  height: "5vh",
-  width: "15vw",
+  height: "4vh",
+  width: "7vw",
   padding: "0 15px",
   borderradius: "3px",
-  margin: "1rem 0 0 0",
+  margin: "0.5rem 0 0 0",
   color: "white",
   backgroundColor: "#D9001D",
   opacity: 0.7,
@@ -20,8 +20,8 @@ export default function Uploadbtn() {
   const handleClick = () => {};
 
   return (
-    <div css={{ position: "absolute", left: "42%" }}>
-      <Button variant="contained" sx={SignInCss} onClick={handleClick}>
+    <div css={{}}>
+      <Button variant="contained" sx={UploadCss} onClick={handleClick}>
         <Typography
           variant="h6"
           sx={{ textAlign: "center", textTransform: "capitalize" }}
