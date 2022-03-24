6e07dedd00b2c49d3e95d86f9138ae6a5def437e
sungyeon1 <sungyeon0320@gmail.com>
2022-03-25 02:27:45 +0900

    :hammer: Add Buy/Sell button and modal

--- front/src/container/ArtDetail/Content/Title.tsx
+++ front/src/container/ArtDetail/Content/Title.tsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material";
+import { Typography, Box } from "@mui/material";
+import Purchase from "./Purchase";
 
 const artwork = {
   title: "Title",
@@ -9,14 +10,21 @@ const artwork = {
 
 export default function Title() {
   return (
-    <>
-      <div css={{ fontSize: "2em", fontWeight: "500", marginBottom: "0.2em" }}>
-        {artwork.title}
-      </div>
-      <Typography component="div" variant="body2" color="text.secondary">
-        created by {artwork.creator_id} | {artwork.created_at} | views{" "}
-        {artwork.views}
-      </Typography>
-    </>
+    <Box sx={{ display: "flex" }}>
+      <Box>
+        <div
+          css={{ fontSize: "2em", fontWeight: "500", marginBottom: "0.2em" }}
+        >
+          {artwork.title}
+        </div>
+        <Typography component="div" variant="body2" color="text.secondary">
+          created by {artwork.creator_id} | {artwork.created_at} | views{" "}
+          {artwork.views}
+        </Typography>
+      </Box>
+      <Box sx={{ margin: "auto 0 auto auto" }}>
+        <Purchase />
+      </Box>
+    </Box>
   );
 }
