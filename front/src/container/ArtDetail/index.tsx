b3a994ae27832ca1cf2e0e55e6b04d06ffe75eca
sungyeon1 <sungyeon0320@gmail.com>
2022-03-28 22:59:43 +0900

    :lipstick: Add layout

--- front/src/container/ArtDetail/index.tsx
+++ front/src/container/ArtDetail/index.tsx
@@ -1,15 +1,18 @@
 import Box from "@mui/material/Box";
 
+import Layout from "@/components/Layout/index";
 import ArtContent from "@/container/ArtDetail/Content/index";
 import ArtistProfile from "@/container/ArtDetail/UserProfile/index";
 
 export default function Artdetail() {
   return (
-    <div css={{ width: "80%", margin: "2rem auto 0 auto" }}>
-      <Box sx={{ mb: "4rem" }}>
-        <ArtContent />
-      </Box>
-      <ArtistProfile />
-    </div>
+    <Layout>
+      <div css={{ width: "80%", margin: "2rem auto 0 auto" }}>
+        <Box sx={{ mb: "4rem" }}>
+          <ArtContent />
+        </Box>
+        <ArtistProfile />
+      </div>
+    </Layout>
   );
 }
--- front/src/container/EntryDetail/index.tsx
+++ front/src/container/EntryDetail/index.tsx
@@ -1,15 +1,18 @@
 import Box from "@mui/material/Box";
 
+import Layout from "@/components/Layout/index";
 import Content from "@/container/EntryDetail/Content/index";
 import ArtistProfile from "@/container/EntryDetail/ArtistProfile/index";
 
 export default function Entrydetail() {
   return (
-    <div css={{ width: "80%", margin: "2rem auto 0 auto" }}>
-      <Box sx={{ mb: "4rem" }}>
-        <Content />
-      </Box>
-      <ArtistProfile />
-    </div>
+    <Layout>
+      <div css={{ width: "80%", margin: "2rem auto 0 auto" }}>
+        <Box sx={{ mb: "4rem" }}>
+          <Content />
+        </Box>
+        <ArtistProfile />
+      </div>
+    </Layout>
   );
 }
