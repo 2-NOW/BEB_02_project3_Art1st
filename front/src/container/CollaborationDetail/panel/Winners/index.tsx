ad3a2fd6f5d6badd3e129c31710d84324a40003d
sungyeon1 <sungyeon0320@gmail.com>
2022-03-31 16:25:23 +0900

    :lipstick: Added collaboration tab in navbar

--- front/src/container/CollaborationDetail/panel/Winners/index.tsx
+++ front/src/container/CollaborationDetail/panel/Winners/index.tsx
@@ -1,15 +1,10 @@
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-
 import ItemList from "./ItemList";
-import data from "@/data/index";
 
 function index() {
   return (
-    <Box sx={{ mt: "2rem" }}>
+    <>
       <ItemList />
-    </Box>
+    </>
   );
 }
 
